0a55b5229933ce380067efc55a8daf628d27c86f
Fernando Pinheiro Campos <fpinheirocampos@gmail.com>
2018-05-08 22:30:45 -0300

    Lesson 2 - step 2

--- Lessons2/RProgramming_Lesson2.r
+++ Lessons2/RProgramming_Lesson2.r
@@ -13,3 +13,23 @@ pollutantmean <- function(directory, pollutant, id = 1:332){
 	values <- vpollutant[!isNA]
 	return(mean(values))
 }
+
+complete <- function(directory, id = 1:332){
+  path <- file.path(getwd(), directory)
+  
+  result <- data.frame()
+
+  for(sensor in id){
+    f <- paste(formatC(sensor, width=3, flag="0"), ".csv", sep="")
+    file <- file.path(path, f)
+    data <- read.csv(file)
+    nobs <- nrow(data[complete.cases(data), ])
+    
+    df <- data.frame(sensor, nobs)
+    names(df) <- c("id", "nobs")
+    
+    result <- rbind(result, df)
+  }
+  
+  return(result)
+}
